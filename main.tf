382ddae2ca39fb3c08ec2dc44fa0e677033b33fb
ankitraii <manman91@gmail.com>
2024-04-14 17:32:45 -0400

    Webapp Testing

--- main.tf
+++ main.tf
@@ -7,19 +7,72 @@ terraform {
             name = "learn-tfc-ar-wsp"
         }
     }
+    required_providers {
+        aws = {
+          source  = "hashicorp/aws"
+          version = "~> 5.45.0"
+        }
+    }
+    required_version = "~> 1.8.0"
 }
 
 provider "aws" {
-    alias = "useast1"
-    region = "us-east-1"
+    region = var.region
 }
 
-# resource "aws_instance" "demo_tfc" {
-#     provider = aws.useast1
-#     ami = "ami-051f8a213df8bc089"
-#     instance_type = "t2.micro"
-# }
+data "aws_ami" "ubuntu" {
+  most_recent = true
 
-# output "instance_id" {
-#     value = aws_instance.demo_tfc.id
-# }
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Amazon
+}
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "deployer-key"
+  public_key = file("${path.module}/key.pub")
+}
+
+resource "aws_instance" "example" {
+  ami                    = data.aws_ami.ubuntu.id
+  key_name               = aws_key_pair.deployer.key_name
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.sg_ssh.id]
+  user_data              = <<-EOF
+              #!/bin/bash
+              apt-get update
+              apt-get install -y apache2
+              sed -i -e 's/80/8080/' /etc/apache2/ports.conf
+              echo "Hello World" > /var/www/html/index.html
+              systemctl restart apache2
+              EOF
+  tags = {
+    Name          = "terraform-learn-state-ec2"
+    drift_example = "v1"
+  }
+}
+
+resource "aws_security_group" "sg_web" {
+  name = "sg_web"
+  ingress {
+    from_port   = "8080"
+    to_port     = "8080"
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  // connectivity to ubuntu mirrors is required to run `apt-get update` and `apt-get install apache2`
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,4 @@
+variable "region" {
+  description = "The AWS region where the resources will be deployed"
+  default = "us-east-1"
+}
